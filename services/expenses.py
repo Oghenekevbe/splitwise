c01b611481ab31ba705784bfd116722bd1494de9
Nazzcodek <bellnas09@gmail.com>
2024-05-06 17:50:40 +0100

    main module to run the app

--- services/expenses.py
+++ services/expenses.py
@@ -1,119 +1,57 @@
-from .users import User, user_instances
-
-ExpenseManager = {}
+from users import User
 
 
 class Expense:
-    def __init__(self, paid_by: User, amount, user_count ,splits,percentages,split_type):
-
+    def __init__(self, paid_by, amount, users, split_type, split_values=None):
         self.paid_by = paid_by
         self.amount = amount
-        self.user_count = user_count
-        self.splits = splits  # the list of people you're splitting by
-        self.percentages = percentages
+        self.users = users
         self.split_type = split_type
-
-
-
+        self.split_values = split_values
 
     def split_expense(self):
-        paid_by = self.paid_by.user_id
-        splits = [user.user_id for user in self.splits]
-        percentages = self.percentages
+        if self.split_type == 'EQUAL':
+            split_amount = self.amount / len(self.users)
+            for user in self.users:
+                if user.user_id != self.paid_by:
+                    self._update_balances(self.paid_by, user.user_id, split_amount)
+        elif self.split_type == 'EXACT':
+            for i, user in enumerate(self.users):
+                if user.user_id != self.paid_by:
+                    self._update_balances(self.paid_by, user.user_id, self.split_values[i])
+        elif self.split_type == 'PERCENT':
+            for i, user in enumerate(self.users):
+                if user.user_id != self.paid_by:
+                    split_amount = self.amount * (self.split_values[i] / 100)
+                    self._update_balances(self.paid_by, user.user_id, split_amount)
 
-        if self.split_type == "EQUAL":
-            self.percentages = None
-            split_amount = round(self.amount / (len(splits)), 2)
-            for user_id in splits:
-                if user_id != paid_by:
-                    # Get the current balance of users before the transaction
-                    paid_by_user = user_instances[paid_by]
-                    paid_by_balance_before = paid_by_user.balance
-                    
-                    user = user_instances[user_id]
-                    user_balance_before = user.balance
+    def _update_balances(self, paid_by, user_id, split_amount):
+        paid_by_user = User.user_instances[paid_by]
+        user = User.user_instances[user_id]
 
-                    # Update the balances
-                    paid_by_user.balance -= split_amount
-                    user.balance += split_amount
-
-                    # Get the current balance of users after the transaction
-                    paid_by_balance_after = paid_by_user.balance
-                    user_balance_after = user.balance
-
-                    # Append the string to the list with the updated balance
-                    if paid_by not in ExpenseManager:
-                        ExpenseManager[paid_by] = []
-                    ExpenseManager[paid_by].append(
-                        f"{user_id} owes {paid_by}: {user_balance_after} ({user_balance_before}+{split_amount})"
-                    )
-
-        elif self.split_type == "EXACT":
-            if len(splits) != len(percentages):
-                raise ValueError("Number of splits must match number of exact amounts")
-            if sum(percentages) != self.amount:
-                raise ValueError("Sum of exact amounts must be equal to total amount")
-                
-            for i, user_id in enumerate(splits):
-                if user_id != paid_by:
-                    split_amount = percentages[i]
-                    # Get the current balance of users before the transaction
-                    paid_by_user = user_instances[paid_by]
-                    paid_by_balance_before = paid_by_user.balance
-                    
-                    user = user_instances[user_id]
-                    user_balance_before = user.balance
-
-                    # Update the balances
-                    paid_by_user.balance -= split_amount
-                    user.balance += split_amount
-
-                    # Get the current balance of users after the transaction
-                    paid_by_balance_after = paid_by_user.balance
-                    user_balance_after = user.balance
-
-                    # Append the string to the list with the updated balance
-                    if paid_by not in ExpenseManager:
-                        ExpenseManager[paid_by] = []
-                    ExpenseManager[paid_by].append(
-                        f"{user_id} owes {paid_by}: {split_amount} (0+{split_amount})"
-                    )
-
-        elif self.split_type == "PERCENT":
-            if percentages is None:
-                raise ValueError("Percentages must be provided for 'PERCENT' split type")
-            if len(percentages) != self.user_count:
-                raise ValueError("Number of percentages must match number of splits")
-            if sum(percentages) != 100:
-                raise ValueError("Percentages must sum up to 100")
-            
-            for i, user_id in enumerate(splits):
-                if user_id != paid_by:
-                    split_amount = round(self.amount * (percentages[i] / 100), 2)
-                    # Get the current balance of users before the transaction
-                    paid_by_user = user_instances[paid_by]
-                    paid_by_balance_before = paid_by_user.balance
-                    
-                    user = user_instances[user_id]
-                    user_balance_before = user.balance
-
-                    # Update the balances
-                    paid_by_user.balance -= split_amount
-                    user.balance += split_amount
-
-                    # Get the current balance of users after the transaction
-                    paid_by_balance_after = paid_by_user.balance
-                    user_balance_after = user.balance
-
-                    # Append the string to the list with the updated balance
-                    if paid_by not in ExpenseManager:
-                        ExpenseManager[paid_by] = []
-                    ExpenseManager[paid_by].append(
-                        f"{user_id} owes {paid_by}: {split_amount} (0+{split_amount})"
-                    )
-
-        latest_expense = ExpenseManager[paid_by][-(len(splits) - 1) :]
-        # Convert the list to a string
-        latest_expense_str = "\n".join(latest_expense)
-        print(latest_expense_str)
-        return latest_expense_str
+        # Check if user already owes paid_by_user
+        if user_id in paid_by_user.owes:
+            owed_amount = paid_by_user.owes[user_id]
+            if owed_amount > split_amount:
+                paid_by_user.owes[user_id] -= split_amount
+                user.is_owed[paid_by] -= split_amount
+            else:
+                del paid_by_user.owes[user_id]
+                del user.is_owed[paid_by]
+                paid_by_user.is_owed[user_id] = split_amount - owed_amount
+                user.owes[paid_by] = split_amount - owed_amount
+        # Check if paid_by_user already owes user
+        elif paid_by in user.owes:
+            owed_amount = user.owes[paid_by]
+            if owed_amount > split_amount:
+                user.owes[paid_by] -= split_amount
+                paid_by_user.is_owed[user_id] -= split_amount
+            else:
+                del user.owes[paid_by]
+                del paid_by_user.is_owed[user_id]
+                user.is_owed[paid_by] = split_amount - owed_amount
+                paid_by_user.owes[user_id] = split_amount - owed_amount
+        # If neither user owes the other, then simply update the balances
+        else:
+            paid_by_user.is_owed[user_id] = split_amount
+            user.owes[paid_by] = split_amount
